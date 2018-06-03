edc12ab7143fb4eb40741d5913f68ee8bbcf24fe
Jun Zhang <zjustc@gmail.com>
2018-06-03 17:48:23 -0400

    dfs

---
+++ solutions/dfs/135. Combination Sum.cpp
@@ -0,0 +1,23 @@
+/* 
+135. Combination Sum
+Description 
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The solution set must not contain duplicate combinations.
+Have you met this question in a real interview?  Yes
+Example
+Given candidate set [2,3,6,7] and target 7, a solution set is:
+
+[7]
+[2, 2, 3]
+
+*/
+
+解题思路:
+直觉上这道题有点类似于二叉树的的path sum 问题, 只是输入数据不是显式的树
+https://www.lintcode.com/problem/binary-tree-path-sum/description
+
